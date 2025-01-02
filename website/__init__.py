74eb4fc480dac1551d5bb6edfd29028ce954b2f3
Alex <139545638+AlexAlexandru20@users.noreply.github.com>
2025-01-02 16:42:51 +0200

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -23,7 +23,7 @@ mail = Mail()
 
 #Create APP
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', template_folder='templates')
     app.config.from_object('config.Config')
     db.init_app(app)
     #init Mail
